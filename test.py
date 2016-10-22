cce88fb8b75c178c8fa909a3dee4773a21cb20fc
LuisGi93 <luisgguijarro9@gmail.com>
2016-10-22 18:57:52 +0200

    Corrigiendo error en fichero de travis.

--- test.py
+++ test.py
@@ -13,31 +13,31 @@ class TestParticipant(unittest.TestCase):
         db.create_all()
 
     def test_receta(self):
-	print "Receta:"
-	size= Receta.query.all()
+	    print "Receta:"
+	    size= Receta.query.all()
         receta = Receta('Lasana bolonesa', 'c4@c4.com','Canelones, cerne, bechamel')
-        db.session.add(receta)     
+        db.session.add(receta)
         db.session.commit()
         recetas = Receta.query.all()
         assert receta in recetas
-	print "  Inserccion: ok"
+	    print "  Inserccion: ok"
         assert len(recetas)==len(size)+1
         print "  Numero entradas: ok"
- 
+
 
     def test_usuario(self):
-	print "Usuario:"
-	size= User.query.all()
+	    print "Usuario:"
+	    size= User.query.all()
         usuario = User("pepe","pepe@pepe.com","123423", 'user', '0')
-        db.session.add(usuario)     
+        db.session.add(usuario)
         db.session.commit()
         usuarios = User.query.all()
         assert usuario in usuarios
-	print "  Inserccion: ok"
+	    print "  Inserccion: ok"
         assert len(usuarios)==len(size)+1
         print "  Numero entradas: ok"
 
 
-        
+
 if __name__ == '__main__':
     unittest.main()
